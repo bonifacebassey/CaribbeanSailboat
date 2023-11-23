045fea2e2c21add82a5aad94426ab269a6afe781
Boniface Bassey <bbasseyboniface@gmail.com>
2023-11-23 21:02:40 +0100

    Added component "Indicator" (will be used to indicate DB connection status)

---
+++ Components/Indicator.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CaribbeanSailboat.Components;
+
+public enum IndicatorColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Default
+}
+
+public partial class Indicator
+{
+    [Parameter]
+    public IndicatorColor Color { get; set; }
+
+    [Parameter]
+    public string? Label { get; set; }
+
+    [Parameter]
+    public string? Height { get; set; }
+
+    [Parameter]
+    public string? Width { get; set; }
+
+    private string Activate => Color switch
+    {
+        IndicatorColor.Red => "led-red",
+        IndicatorColor.Green => "led-green",
+        IndicatorColor.Blue => "led-blue",
+        IndicatorColor.Yellow => "led-yellow",
+        IndicatorColor.Orange => "led-orange",
+        _ => "",
+    };
+}
