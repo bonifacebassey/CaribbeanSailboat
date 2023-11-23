12a0d80eb2a18ea14ebb5104d83ec7d325a114a9
Boniface Bassey <bbasseyboniface@gmail.com>
2023-11-23 21:54:37 +0100

    Created reusable component "StandardButton"

---
+++ Components/StandardButton.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CaribbeanSailboat.Components;
+
+public partial class StandardButton
+{
+    [Parameter]
+    public EventCallback Click { get; set; }
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    [Parameter]
+    public bool IsEnabled { get; set; } = true;
+
+    [Parameter]
+    public string? CssStyle { get; set; }
+
+    [Parameter]
+    public string? Color { get; set; }
+
+    [Parameter]
+    public string? Background { get; set; }
+
+    [Parameter]
+    public string? Radius { get; set; }
+
+    protected void OnClick()
+    {
+        if (Click.HasDelegate)
+        {
+            _ = Click.InvokeAsync();
+        }
+    }
+}
